a6c87bdeebd0063201f02796d7ddd9465dbd30e0
volkantash <djangoogreniyorum@gmail.com>
2025-05-02 04:53:04 +0300

    Pazaryeri bilgisi eklendi.

--- kargo/admin.py
+++ kargo/admin.py
@@ -4,6 +4,15 @@ from django.contrib import admin
 
 from .models import *
 
+@admin.register(KargoUcreti)
+class KargoUcretiAdmin(admin.ModelAdmin):
+    list_display = ['pazaryeri', 'kargo_firmasi', 'desi', 'ucret']
+
+    @admin.display(description="Kargo Bilgisi")
+    def kargo_bilgisi(self, obj):
+        return str(obj)
+
+
 admin.site.register(KargoFirmasi)
-admin.site.register(KargoUcreti)
 admin.site.register(KargoVergiOrani)
+admin.site.register(Pazaryeri)
--- kargo/models.py
+++ kargo/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 
+class KargoVergiOrani(models.Model):
+    vergi_orani = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = "Kargo Vergisi Oranı"
+        verbose_name_plural = 'Kargo Vergisi Oranları'
+
+    def __str__(self):
+        return f"Vergi Oranı: {self.vergi_orani}%"
 
 class KargoFirmasi(models.Model):
     ad = models.CharField(max_length=255, unique=True)
@@ -15,10 +24,23 @@ class KargoFirmasi(models.Model):
     def __str__(self):
         return self.ad
 
+class Pazaryeri(models.Model):
+    ad = models.CharField(max_length=255, unique=True)
+    etkin_mi = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Pazaryeri"
+        verbose_name_plural = 'Pazaryerleri'
+
+    def __str__(self):
+        return self.ad
+
 
 class KargoUcreti(models.Model):
     kargo_firmasi = models.ForeignKey(
-        KargoFirmasi, on_delete=models.CASCADE, related_name='ucretler')
+        KargoFirmasi, on_delete=models.CASCADE, related_name='kargo_ucretleri')
+    pazaryeri = models.ForeignKey(
+        Pazaryeri, on_delete=models.CASCADE, related_name='pazaryeri_ucretleri', blank=True, null=True)
     desi = models.PositiveIntegerField()
     ucret = models.DecimalField(max_digits=10, decimal_places=2)
 
@@ -32,12 +54,6 @@ class KargoUcreti(models.Model):
         return f"{self.kargo_firmasi.ad} - {self.desi} desi: {self.ucret} TL"
 
 
-class KargoVergiOrani(models.Model):
-    vergi_orani = models.DecimalField(max_digits=5, decimal_places=2)
 
-    class Meta:
-        verbose_name = "Kargo Vergisi Oranı"
-        verbose_name_plural = 'Kargo Vergisi Oranları'
 
-    def __str__(self):
-        return f"Vergi Oranı: {self.vergi_orani}%"
+
